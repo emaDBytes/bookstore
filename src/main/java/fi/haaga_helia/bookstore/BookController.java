2d6c8e8452bd30a3bc8596c9ba2ed350ed8e200c
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-10-30 20:17:28 +0200

    Add BookController with index endpoint

---
+++ src/main/java/fi/haaga_helia/bookstore/BookController.java
@@ -0,0 +1,13 @@
+package fi.haaga_helia.bookstore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+    
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
