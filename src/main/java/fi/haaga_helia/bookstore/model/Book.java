9bbb3305aa978b11984e19a618ca7b62887b8c76
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-10-30 21:14:45 +0200

    Convert Book class to JPA entity

--- src/main/java/fi/haaga_helia/bookstore/model/Book.java
+++ src/main/java/fi/haaga_helia/bookstore/model/Book.java
@@ -1,6 +1,16 @@
 package fi.haaga_helia.bookstore.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
     private String title;
     private String author;
     private int publicationYear;
@@ -19,42 +29,50 @@ public class Book {
         this.price = price;
     }
 
-    // Getters
+    // Getter and setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Other getters and setters remain the same
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getAuthor() {
         return author;
     }
 
-    public int getPublicationYear() {
-        return publicationYear;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    // Setters
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
     }
 
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
 
+    public String getIsbn() {
+        return isbn;
+    }
+
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
     public void setPrice(double price) {
