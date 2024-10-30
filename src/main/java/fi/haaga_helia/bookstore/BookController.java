dfded7b61d757637dddc09ee820719618427124f
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-10-30 22:47:02 +0200

    Add booklist template and endpoint to display all books

--- src/main/java/fi/haaga_helia/bookstore/BookController.java
+++ src/main/java/fi/haaga_helia/bookstore/BookController.java
@@ -1,13 +1,26 @@
 package fi.haaga_helia.bookstore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import fi.haaga_helia.bookstore.repository.BookRepository;
 
 @Controller
 public class BookController {
+
+    @Autowired
+    private BookRepository repository;
     
     @GetMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
