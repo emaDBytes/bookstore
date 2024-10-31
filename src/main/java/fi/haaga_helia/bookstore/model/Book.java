f2dede3f11094357125f1e36cbbd0a03c8974af4
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-10-31 14:56:33 +0200

    Add JsonIgnore annotation to prevent infinite recursion in REST response

--- src/main/java/fi/haaga_helia/bookstore/model/Book.java
+++ src/main/java/fi/haaga_helia/bookstore/model/Book.java
@@ -1,5 +1,7 @@
 package fi.haaga_helia.bookstore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +23,7 @@ public class Book {
 
     @ManyToOne
     @JoinColumn(name = "categoryid")
+    @JsonIgnore
     private Category category;
 
     // Default constructor
