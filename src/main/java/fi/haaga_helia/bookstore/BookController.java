e9fc1fe530847a11c3082b079aa1ac52af60db37
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-10-31 08:53:27 +0200

    Add REST endpoint to get all books

--- src/main/java/fi/haaga_helia/bookstore/BookController.java
+++ src/main/java/fi/haaga_helia/bookstore/BookController.java
@@ -1,10 +1,13 @@
 package fi.haaga_helia.bookstore;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 
 import fi.haaga_helia.bookstore.model.Book;
@@ -16,10 +19,11 @@ public class BookController {
     @Autowired
     private BookRepository repository;
     
-    // @GetMapping("/index")
-    // public String index() {
-    //     return "index";
-    // }
+    // Original index endpoint
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
 
     // List all books
     @GetMapping("/booklist")
@@ -47,5 +51,12 @@ public class BookController {
         repository.deleteById(bookId);
         return "redirect:../booklist";
     }
-    
+ 
+    // REST endpoint to get all books
+    @GetMapping("/api/books")
+    @ResponseBody
+    public List<Book> bookListRest() {
+        return (List<Book>) repository.findAll();
+    }
+
 }
