a9be2b1c02e0132fc487a80ebfd032163c917ff7
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-11-06 19:21:11 +0200

    Fix Delete button visibility based on user role

--- src/main/java/fi/haaga_helia/bookstore/config/WebSecurityConfig.java
+++ src/main/java/fi/haaga_helia/bookstore/config/WebSecurityConfig.java
@@ -14,45 +14,45 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(antMatcher("/**")).authenticated())
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/booklist", true)
-                        .permitAll())
-                .logout(logout -> logout
-                        .permitAll());
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers("/delete/**").hasRole("ADMIN")
+                                                .anyRequest().authenticated())
+                                .formLogin(form -> form
+                                                .loginPage("/login")
+                                                .defaultSuccessUrl("/booklist", true)
+                                                .permitAll())
+                                .logout(logout -> logout
+                                                .permitAll());
+                return http.build();
+        }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        System.out.println("Creating in-memory users - two users will be created");
-        List<UserDetails> users = new ArrayList<UserDetails>();
+        @Bean
+        public UserDetailsService userDetailsService() {
+                System.out.println("Creating in-memory users - two users will be created");
+                List<UserDetails> users = new ArrayList<UserDetails>();
 
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+                PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-        UserDetails user1 = User.withUsername("user")
-                .password(passwordEncoder.encode("user"))
-                .roles("USER")
-                .build();
-        users.add(user1);
+                UserDetails user1 = User.withUsername("user")
+                                .password(passwordEncoder.encode("user"))
+                                .roles("USER")
+                                .build();
+                users.add(user1);
 
-        UserDetails user2 = User.withUsername("admin")
-                .password(passwordEncoder.encode("admin"))
-                .roles("USER", "ADMIN")
-                .build();
-        users.add(user2);
+                UserDetails user2 = User.withUsername("admin")
+                                .password(passwordEncoder.encode("admin"))
+                                .roles("USER", "ADMIN")
+                                .build();
+                users.add(user2);
 
-        return new InMemoryUserDetailsManager(users);
-    }
+                return new InMemoryUserDetailsManager(users);
+        }
 }
