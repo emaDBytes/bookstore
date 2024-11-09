5a7af2c8c876590c9673ffb29c32cf1e87ce7557
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-11-09 12:41:19 +0200

    Update security config to use database authentication

--- src/main/java/fi/haaga_helia/bookstore/config/WebSecurityConfig.java
+++ src/main/java/fi/haaga_helia/bookstore/config/WebSecurityConfig.java
@@ -1,19 +1,15 @@
 package fi.haaga_helia.bookstore.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import fi.haaga_helia.bookstore.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -34,25 +30,16 @@ public class WebSecurityConfig {
                 return http.build();
         }
 
+        @Autowired
+        private UserDetailsServiceImpl userDetailsService;
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        }
+
         @Bean
-        public UserDetailsService userDetailsService() {
-                System.out.println("Creating in-memory users - two users will be created");
-                List<UserDetails> users = new ArrayList<UserDetails>();
-
-                PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-                UserDetails user1 = User.withUsername("user")
-                                .password(passwordEncoder.encode("user"))
-                                .roles("USER")
-                                .build();
-                users.add(user1);
-
-                UserDetails user2 = User.withUsername("admin")
-                                .password(passwordEncoder.encode("admin"))
-                                .roles("USER", "ADMIN")
-                                .build();
-                users.add(user2);
-
-                return new InMemoryUserDetailsManager(users);
+        public BCryptPasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
         }
 }
